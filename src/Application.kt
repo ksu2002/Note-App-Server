44542d34fee4de7ec0f42566c07304cf7d0792b2
ksu2002 <simonova278@gmail.com>
2023-02-16 00:04:25 +0500

    Revert "added JWT Authentication"
    This reverts commit 667d3e47bc01b116f5eb6b272300c2d16599817f.

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,6 @@
 package com.example
 
-import com.example.authentication.JwtService
-import com.example.authentication.hash
-import com.example.data.model.User
 import com.example.repository.DatabaseFactory
-import com.example.repository.repo
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
@@ -22,10 +18,6 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
 
     DatabaseFactory.init()
-
-    val db = repo()
-    val jwtService = JwtService()
-    val hashFunction = {s:String -> hash(s)}
 
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
@@ -55,22 +47,6 @@ fun Application.module(testing: Boolean = false) {
         get("/note") {
             val id = call.request.queryParameters["id"]
             call.respond("${id}")
-        }
-        get ("/toke"){
-            val email =  call.request.queryParameters["email"]!!
-            val password =  call.request.queryParameters["password"]!!
-            val username =  call.request.queryParameters["username"]!!
-
-            val user = User(email, hashFunction(password), username)
-            call.respond("jwtService.generateToken(user)")
-        }
-        get ("/token"){
-            val email =  call.request.queryParameters["email"]!!
-            val password =  call.request.queryParameters["password"]!!
-            val username =  call.request.queryParameters["username"]!!
-
-            val user = User(email, hashFunction(password), username)
-            call.respond(jwtService.generateToken(user))
         }
 
         route("/notes") {
